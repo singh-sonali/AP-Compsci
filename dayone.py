5ead1e5e4746f0c64b2f4119b279af9ffd6d107c
Sonali Singh <ssingh20@choate.edu>
2018-09-08 15:25:03 -0400

    Added more lines of the computer conversation.

--- dayone.py
+++ dayone.py
@@ -1,3 +1,18 @@
-print("Hello, world!")
-username = input("What's your name? ")
-print("Greetings, " + username + "!")
+# September 8, 2018
+# Sonali's Computer Conversation
+# No outside sources.
+print("Hello!")
+username = input("What would you like me to call you? ")
+print("Well, hello there " + username + "!")
+feeling = input("How are you feeling today? ")
+print("Nice to hear you're feeling " + feeling + ", " + username + "!")
+print("Let's get to know each other.")
+color = input("What's your favorite color? ")
+print("Isn't that weird? My favorite color is " + color + " too!")
+origin = input("Where are you from " + username + "? ")
+print("Nice! I hear " + origin + " is a really beautiful place!")
+food = input("What's the best dish that you've eaten in " + origin + ", " + username + "? ")
+print("Yum!")
+meeting = input("When are you free to talk again? I feel like I know you so well already. ")
+print("Ok, " + username + " I'll talk to you " + meeting + ". Maybe we can meet in " + origin + " and eat some " + food + "! ")
+print("Bye for now!")
