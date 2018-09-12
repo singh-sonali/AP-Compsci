7211db0e34b5338b7997dabd2b4372114ea77b25
Sonali Singh <ssingh20@choate.edu>
2018-09-12 08:03:49 -0400

    updated classwork

---
+++ greetings.py
@@ -0,0 +1,13 @@
+import sys 
+# if you give too many arguments, it's fine, they're just ignored
+#too little arguments gives you an error
+# [0] references the name of the file, because it's technically the first argument
+name = sys.argv[1]
+name2 = sys.argv[2]
+
+#len(sys.argv)
+#this tells how many arguments have been passed
+
+
+
+print("Hello, " + name + " and " + name2 +"!")
---
+++ sys_version.py
@@ -0,0 +1,3 @@
+import sys
+print('version is', sys.version)
+
