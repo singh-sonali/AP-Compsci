71f2c266f955b5d1b46e0a2db9cbe9a00d23bb36
Sonali Singh <ssingh20@choate.edu>
2018-09-17 14:15:05 -0400

    quick edits

--- ifstatement.py
+++ ifstatement.py
@@ -9,29 +9,29 @@ if (g<0 or g>5):
 
 elif (g>= 4.85):
 	print("A+")
-elif (g<4.85 and g>= 4.7):
+elif (g>= 4.7):
 	print("A")
-elif (g<4.7 and g>=4.5):
+elif (g>=4.5):
 	print("A-")
-elif (g<4.5 and g>=4.2):
+elif (g>=4.2):
 	print("B+")
-elif (g<4.2 and g>=3.85):
+elif (g>=3.85):
 	print("B")
-elif(g<3.85 and g>=3.5):
+elif(g>=3.5):
 	print("B-")
-elif(g<3.5 and g>=3.2):
+elif(g>=3.2):
 	print("C+")
-elif(g<3.2 and g>=2.85):
+elif(g>=2.85):
 	print("C")
-elif(g<2.85 and g>=2.5):
+elif(g>=2.5):
 	print("C-")
-elif(g<2.5 and g>=2):
+elif(g>=2):
 	print("D+")
-elif(g<2 and g>=1.5):
+elif(g>=1.5):
 	print("D")
-elif(g<1.5 and g>=1):
+elif(g>=1):
 	print("D-")
 else:
 	print("Sorry, you failed. F.")
-	
+
 
