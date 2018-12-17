3922fb64e5d127d98c214577d565c2f5a0494bb0
Sonali Singh <ssingh20@choate.edu>
2018-12-17 14:15:45 -0500

    Loops practice

--- loops_practice.py
+++ loops_practice.py
@@ -1,3 +1,5 @@
+#Partner 1: Sonali
+#Partner 2: Leo
 ''' Instructions:
    Work with a partner to complete these tasks. Assume that all variables mentioned in the description are declared and initialized; however, feel free to use additional variable as necessary (please avoid extra variables, though; don't use them unless you must to store a required value or simplify your code.) Write your solution below the commented description.
 '''
@@ -168,32 +170,53 @@ print("Result:",exponent)
 ''' 14. 
    Write a loop that will print out all the letters of the alphabet.
 '''
+import string
+
+letters = list(string.ascii_lowercase)
+for i in range(0, 26):
+    print (letters[i])
  
  
 ''' 15. 
    Now write a loop that will print out "A is a vowel." "B is a consonant." "C is a consonant." and so on. 
 '''
- 
+letterss = list(string.ascii_uppercase)
+for i in range(len(letterss)):
+  if (i==0 or i==4 or i==8 or i==14 or i==20):
+    print(letterss[i], "is a "+"vowel")
+  else:
+    print(letterss[i], "is a "+"consonant")
  
  
 ''' 16. 
    Write code that will produce the following output: 
    122333444455555666666777777788888888999999999
 '''
- 
+numbers = []
+for i in range(1, 10):
+  for j in range(i):
+    numbers.append(i)
+a = "".join(map(str, numbers))
+print(a)
  
  
 ''' 17. 
    Write a loop that will print out the decimal equivalents of 1/2, 1/3, 1/4, 1/5, 1/6, ... 1/20. The output for each iteration should look like:
    "1/2 = .5" "1/3 = .666666666667" etc.
 '''
+for i in range(2, 21):
+  print("1/"+str(i)+" = "+str(1/i))
  
  
  
 ''' 18. 
    Write a loop that determines the sum of all the numbers from 1-100, as well as the average. Store the sum in variable total (int) and the average in variable avg (float).
 '''
- 
+total = 0
+for i in range(0, 101):
+  total+=i
+avg = total/i
+print(total, avg)
  
  
 ''' 19. 
@@ -201,20 +224,33 @@ print("Result:",exponent)
    PI = 4 * (1-1/3+1/5-1/7+1/9-1/11+1/13-1/15...)
    Write a loop that will calculate this output for n-iterations of the pattern (n being an int), that could help you determine if your friend is right or wrong.
 '''
- 
+for i in range(10):
+  PI = 4
+  PI += 4*(-1**i*1/(2*(i+1)-1))
+  print(PI)
  
  
 ''' 20. 
    A mother rabbit can have a litter of rabbits every month. In the litter, the number of rabbits can vary from 1 to 14 babies per litter, half of which are females. Rabbits can start reproducing at 6 months, so let's add all the new rabbits from the year to the reproductive pool at the end of each year (when their average age is 6 months). Write a simulation that will show how many rabbits will exist at the end of 5 years, starting with just one mother rabbit. 
 '''
- 
+#interpreted differently
+import random
+mother = 1
+offspring = 0
+for reproductions in range(0, 10, 1):
+  for m in range(mother):
+    children = random.randrange(1, 15)
+    offspring+=children
+    mother = 0
+    mother += (children//2)
+    print("mothers", mother)
+print("Number of rabbits:", offspring)
  
  
 ''' 21. 
    Write some code that will run the rabbit simulation above 1000 times, to help determine what we can expect on average.
 '''
- 
- 
+ #not clear??
  
 ''' 22. 
    Write a loop which prints the numbers 1 to 110, 11 numbers per line. The program shall print "Coza" in place of the numbers which are multiples of 3, "Loza" for multiples of 5, "Woza" for multiples of 7, "CozaLoza" for multiples of 3 and 5, and so on. Sample output:
@@ -223,7 +259,57 @@ print("Result:",exponent)
    23 Coza Loza 26 Coza Woza 29 CozaLoza 31 32 Coza
    ......
 '''
- 
+numberes = []
+numberss = []
+for i in range(10):
+  numberss = []
+  numberes.append(numberss)
+  for j in range(1, 12):
+    if i == 0:
+        numberss.append(j)
+    if i == 1:
+        numberss.append(11+j)
+    if i == 2:
+        numberss.append(22+j)
+    if i == 3:
+        numberss.append(33+j)
+    if i == 4:
+        numberss.append(44+j)
+    if i == 5:
+        numberss.append(55+j)
+    if i == 6:
+        numberss.append(66+j)
+    if i == 7:
+        numberss.append(77+j)
+    if i == 8:
+        numberss.append(88+j)
+    if i == 9:
+        numberss.append(99+j)
+
+for i in range(10):
+    for j in range(11):
+      print(numberes[i][j])
+      print(numberes[i][j]%3 == 0)
+      if numberes[i][j]%3 == 0 and numberes[i][j]%5 == 0 and numberes[i][j]%7 == 0:
+        numberes[i][j] = "CozaLozaWoza"
+      elif numberes[i][j]%3 == 0 and numberes[i][j]%5 == 0:
+        numberes[i][j] = "CozaLoza"
+      elif numberes[i][j]%3 == 0 and numberes[i][j]%7 == 0:
+        numberes[i][j] = "CozaWoza"
+      elif numberes[i][j]%5 == 0 and numberes[i][j]%7 == 0:
+        numberes[i][j] = "LozaWoza"
+      elif numberes[i][j]%3 == 0:
+        numberes[i][j] = "Coza"
+      elif numberes[i][j]%5 == 0:
+        numberes[i][j] = "Loza"
+      elif numberes[i][j]%7 == 0:
+        numberes[i][j] = "Woza"
+
+for row in numberes:
+  for elem in row:
+    print(elem, end = ' ')
+  print()
+
  
  
 ''' 23.
